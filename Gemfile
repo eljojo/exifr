d02c1be21c87d759122222cbb6cae0a9994926f0
José Tomás Albornoz <jojo@eljojo.net>
2013-01-26 18:05:51 +0100

    showing more info

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'http://rubygems.org'
 gem 'sinatra'
 gem 'exifr'
+gem 'andand'
+gem 'sinatra-contrib'
+
--- hello.rb
+++ hello.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
+require "sinatra/json"
 require 'exifr'
 require 'open-uri'
-require 'pp'
+require 'andand'
+
 get '/' do
   foto = open('http://24.media.tumblr.com/3bfe00f9d4933992a8e7e97f9878b13f/tumblr_mh8q7oN4BG1rianpko1_1280.jpg')
   exif = EXIFR::JPEG.new(foto)
-  "#{exif.gps.latitude} #{exif.gps.longitude}"
+  result = {
+    latitude:   exif.gps.andand.latitude || '',
+    longitude:  exif.gps.andand.longitude || '',
+    width:      exif.width || '',
+    height:     exif.height || '',
+    model:      exif.model || '',
+    # date_time:  exif.date_time || '',
+    exposure:   exif.exposure_time.to_s || '',
+    f_number:   exif.f_number.to_f || '',
+  }
+  json result
 end
