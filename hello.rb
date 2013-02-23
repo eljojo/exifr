85f9e24bf275f499367adcd15e0c12bceff511a0
José Tomás Albornoz <jojo@eljojo.net>
2013-02-23 15:48:49 +0100

    working

--- hello.rb
+++ hello.rb
@@ -5,17 +5,29 @@ require 'open-uri'
 require 'andand'
 
 get '/' do
-  foto = open('http://24.media.tumblr.com/3bfe00f9d4933992a8e7e97f9878b13f/tumblr_mh8q7oN4BG1rianpko1_1280.jpg')
-  exif = EXIFR::JPEG.new(foto)
-  result = {
-    latitude:   exif.gps.andand.latitude || '',
-    longitude:  exif.gps.andand.longitude || '',
+  if (/https?:\/\/.+\.jpg/i).match(params_url) then
+    json process_image
+  else
+    json error: "there's no jpeg and stuff", url: params_url
+  end
+end
+
+def params_url
+  request.fullpath.slice(2, request.fullpath.length - 2)
+end
+
+def process_image
+  exif = EXIFR::JPEG.new(open(params_url))
+  {
     width:      exif.width || '',
     height:     exif.height || '',
     model:      exif.model || '',
     # date_time:  exif.date_time || '',
     exposure:   exif.exposure_time.to_s || '',
     f_number:   exif.f_number.to_f || '',
+    gps: {
+      latitude:   exif.gps.andand.latitude || '',
+      longitude:  exif.gps.andand.longitude || '',
+    }
   }
-  json result
 end
